917107d159eab37e740b3e944f2f73290092aedf
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2026-03-31 12:26:50 +0200

    added script

---
+++ webapp/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+source venv/bin/activate
+pip install -q flask requests python-dateutil
+python3 app.py
